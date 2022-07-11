7086b1ffccc9f90401e12e752a4ee47f7095f369
Michael Okello <okellofrost@gmail.com>
2022-07-11 15:01:22 +0300

    Adding Constructor solutions

--- index.js
+++ index.js
@@ -4,20 +4,34 @@
 //Zipcode
 //create a fucntion showAddress(address) and shows all  its values
 
-let address = {
-    street: 'Kondele',
-    city: 'Kisumu',
-    zip: '40100'
+let address1 = {
+    street:'Namibia',
+    city :'Windhoek',
+    zip:'34535'
 };
 
 
-function showAddress(address2){
+function showAddress(address1){
 
-    for( let key in address){
-        console.log(key,address[key]);
+    for( let key in address1){
+        console.log(key,address1[key]);
         // for (let key of Object.keys(address));
         //   console.log(key);
     }
  
 }
-showAddress(address);
+showAddress(address1);
+
+//Factory Fucntion Answer
+
+let address2 = createAddress('Oginga','Kisumu', '40100');
+console.log(address2);
+
+function createAddress(street, city , zipcode){
+
+    return{
+        street,
+        city ,
+        zipcode
+    };
+}
